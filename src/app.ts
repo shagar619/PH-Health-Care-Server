61f145620d35dcdc49f0e83b26c6880038833304
shagar619 <ashagar619@gmail.com>
2025-11-16 23:33:45 +0600

    added required middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import config from './config';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 
 const app: Application = express();

c020cffc5ae5ed0b5872c8c88d877359e160f398
shagar619 <ashagar619@gmail.com>
2025-11-16 23:28:04 +0600

    implement server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import config from './config';
+
+
+const app: Application = express();
+app.use(cors({
+     origin: 'http://localhost:3000',
+     credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+     res.send({
+          message: "Server is running..",
+          environment: config.node_env,
+          uptime: process.uptime().toFixed(2) + " sec",
+          timeStamp: new Date().toISOString()
+     })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;

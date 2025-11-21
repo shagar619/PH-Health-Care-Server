03939fccac2c2085634b191b151880a009c6cafc
shagar619 <ashagar619@gmail.com>
2025-11-21 22:24:14 +0600

    added create user controller

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { UserService } from "./user.service";
+
+
+const createPatient = catchAsync(async (req: Request, res: Response) => {
+
+     const result = await UserService.createPatient(req.body);
+
+     sendResponse(res, {
+          statusCode: 201,
+          success: true,
+          message: "Patient created successfully!",
+          data: result
+     })
+})
+
+export const UserController = {
+     createPatient
+}

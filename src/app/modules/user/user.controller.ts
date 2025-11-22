6014180c9a1b16e2219372c0efcbf54ce7775945
shagar619 <ashagar619@gmail.com>
2025-11-22 17:09:57 +0600

    added http StatusCodes

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { UserService } from "./user.service";
+import StatusCodes from "http-status-codes";
 
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
@@ -9,7 +10,7 @@ const createPatient = catchAsync(async (req: Request, res: Response) => {
      const result = await UserService.createPatient(req.body);
 
      sendResponse(res, {
-          statusCode: 201,
+          statusCode: StatusCodes.CREATED,
           success: true,
           message: "Patient created successfully!",
           data: result

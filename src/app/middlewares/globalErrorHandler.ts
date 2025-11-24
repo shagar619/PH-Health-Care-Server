1199cd7eaddc702cbc11f6932b69b02cbef7a970
shagar619 <ashagar619@gmail.com>
2025-11-24 21:47:10 +0600

    Prisma Schema for Schedule and Doctor Schedule

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,26 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express"
-import httpStatus from "http-status"
 
-const globalErrorHandler = (err: any, req: Request, res: Response) => {
+import { Request, Response, NextFunction } from "express";
+import httpStatus from "http-status";
+
+const globalErrorHandler = (
+     err: any,
+     req: Request,
+     res: Response,
+     next: NextFunction
+) => {
 
      let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
      let success = false;
-     let message = err.message || "Something went wrong!";
+     let message = err?.message || "Something went wrong!";
      let error = err;
 
-     res.status(statusCode).json({
-          success,
-          message,
-          error
-     })
+res.status(statusCode).json({
+     success,
+     message,
+     error
+});
 };
 
 export default globalErrorHandler;

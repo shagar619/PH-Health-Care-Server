ae143d84250c24c376d73707b3484764b8f55db5
shagar619 <ashagar619@gmail.com>
2025-12-01 22:53:47 +0600

    implement creating appointment

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,9 +1,35 @@
+import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
+import { v4 as uuidv4 } from 'uuid';
 
 
 const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
 
-     return null;
+     const patientData = await prisma.patient.findUniqueOrThrow({
+          where: {
+               email: user.email
+          }
+     });
+
+     const doctorData = await prisma.doctor.findUniqueOrThrow({
+          where: {
+               id: payload.doctorId,
+               isDeleted: false
+          }
+     });
+
+     const isBookedOrNot = await prisma.doctorSchedules.findFirstOrThrow({
+          where: {
+               doctorId: payload.doctorId,
+               scheduleId: payload.scheduleId,
+               isBooked: false
+          }
+     });
+
+     const videoCallingId = uuidv4();
+
+
+
 }
 
 

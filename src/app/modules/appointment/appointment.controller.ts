a1232a15e80e83877d92fd9d1d71bc129d0937a8
shagar619 <ashagar619@gmail.com>
2025-12-03 01:16:08 +0600

    Troubleshooting Issues in Fetching Appointments & Finding Solutions

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -4,6 +4,7 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import StatusCode from "http-status-codes";
 import { AppointmentService } from "./appointment.service";
+import pick from "../../helper/pick";
 
 
 const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -19,6 +20,28 @@ const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload 
      })
 });
 
+
+
+const getMyAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+
+     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+
+     const fillters = pick(req.query, ["status", "paymentStatus"]);
+
+     const user = req.user;
+
+     const result = await AppointmentService.getMyAppointment(user as IJWTPayload, fillters, options);
+
+     sendResponse(res, {
+          statusCode: StatusCode.OK,
+          success: true,
+          message: "Appointment fetched successfully!",
+          data: result
+     })
+})
+
+
 export const AppointmentController = {
      createAppointment,
+     getMyAppointment
 }
--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Prisma, UserRole } from "@prisma/client";
 import { stripe } from "../../helper/stripe";
 import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
 import { v4 as uuidv4 } from 'uuid';
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 
 
 const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
@@ -97,6 +100,68 @@ const createAppointment = async (user: IJWTPayload, payload: { doctorId: string,
 
 
 
+const getMyAppointment = async (user: IJWTPayload, filters: any, options: IOptions) => {
+
+     const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+     const { ...filterData } = filters;
+
+     const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+     if (user.role === UserRole.PATIENT) {
+     andConditions.push({
+          patient: {
+               email: user.email
+          }
+     })
+     }
+     else if (user.role === UserRole.DOCTOR) {
+     andConditions.push({
+          doctor: {
+               email: user.email
+          }
+     })
+     }
+
+     if (Object.keys(filterData).length > 0) {
+     const filterConditions = Object.keys(filterData).map(key => ({
+          [key]: {
+               equals: (filterData as any)[key]
+          }
+     }))
+
+     andConditions.push(...filterConditions)
+     }
+
+     const whereConditions: Prisma.AppointmentWhereInput = andConditions.length > 0 ? { AND: andConditions } : {};
+
+     const result = await prisma.appointment.findMany({
+     where: whereConditions,
+     skip,
+     take: limit,
+     orderBy: {
+          [sortBy]: sortOrder
+     },
+     include: user.role === UserRole.DOCTOR ?
+          { patient: true } : { doctor: true }
+     });
+
+     const total = await prisma.appointment.count({
+          where: whereConditions
+     });
+
+     return {
+     meta: {
+          total,
+          limit,
+          page
+     },
+     data: result
+     }
+}
+
+
+
 export const AppointmentService = {
      createAppointment,
+     getMyAppointment
 };

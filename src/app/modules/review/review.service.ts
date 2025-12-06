235e2dc20f6f04f5e4bf14414cc2e56fcf55d589
shagar619 <ashagar619@gmail.com>
2025-12-06 22:51:34 +0600

    added review services

---
+++ src/app/modules/review/review.service.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import StatusCode from "http-status-codes";
+import ApiError from "../../errors/ApiError";
+import { prisma } from "../../shared/prisma";
+import { IJWTPayload } from "../../types/common";
+
+const insertIntoDB = async (user: IJWTPayload, payload: any) => {
+
+     const patientData = await prisma.patient.findUniqueOrThrow({
+     where: {
+          email: user.email
+     }
+     });
+
+     const appointmentData = await prisma.appointment.findUniqueOrThrow({
+     where: {
+          id: payload.appointmentId
+     }
+     });
+
+     if (patientData.id !== appointmentData.patientId) {
+          throw new ApiError(StatusCode.BAD_REQUEST, "This is not your appointment!")
+     }
+
+     return await prisma.$transaction(async (tnx) => {
+     const result = await tnx.review.create({
+          data: {
+               appointmentId: appointmentData.id,
+               doctorId: appointmentData.doctorId,
+               patientId: appointmentData.patientId,
+               rating: payload.rating,
+               comment: payload.comment
+          }
+     });
+
+     const avgRating = await tnx.review.aggregate({
+          _avg: {
+               rating: true
+          },
+          where: {
+               doctorId: appointmentData.doctorId
+          }
+     })
+
+     await tnx.doctor.update({
+          where: {
+               id: appointmentData.doctorId
+          },
+          data: {
+               averageRating: avgRating._avg.rating as number
+          }
+     })
+
+     return result;
+     })
+};
+
+export const ReviewService = {
+     insertIntoDB
+}

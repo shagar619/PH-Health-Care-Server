3835383df0e8a7c5b007ceafa3d2a1d61119b2e0
shagar619 <ashagar619@gmail.com>
2025-12-08 01:17:28 +0600

    added refreshToken service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 import { jwtHelper } from "../../helper/jwtHelper";
 import StatusCode from "http-status-codes";
 import ApiError from "../../errors/ApiError";
+import config from "../../../config";
+import { Secret } from "jsonwebtoken";
 
 
 
@@ -33,6 +35,41 @@ const login = async (payload: { email: string, password: string }) => {
      }
 }
 
+
+const refreshToken = async (token: string) => {
+
+     let decodedData;
+     try {
+          decodedData = jwtHelper.verifyToken(token, config.jwt.refresh_token_secret as Secret);
+     }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+     catch (err) {
+          throw new Error("You are not authorized!")
+     }
+
+     const userData = await prisma.user.findUniqueOrThrow({
+     where: {
+          email: decodedData.email,
+          status: UserStatus.ACTIVE
+     }
+     });
+
+     const accessToken = jwtHelper.generateToken({
+          email: userData.email,
+          role: userData.role
+     },
+     config.jwt.jwt_secret as Secret,
+     config.jwt.expires_in as string
+     );
+
+     return {
+          accessToken,
+          needPasswordChange: userData.needPasswordChange
+     };
+
+};
+
 export const AuthService = {
-     login
+     login,
+     refreshToken
 }

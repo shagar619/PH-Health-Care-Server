c47efe4048e208f255c65ba334d1f2a98a37ad70
shagar619 <ashagar619@gmail.com>
2025-12-30 02:03:00 +0600

    updated part-3

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,30 +1,29 @@
-import { Router } from 'express';
+/* eslint-disable no-console */
+import { NextFunction, Request, Response, Router } from 'express';
 import { AuthController } from './auth.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
+import { authLimiter } from '../../middlewares/rateLimiter';
 
 
 
 const router = Router();
 
-router.get(
-     "/me",
-     AuthController.getMe
-)
-
 router.post(
      "/login",
+     authLimiter,
      AuthController.login
-)
+);
 
 router.post(
      '/refresh-token',
      AuthController.refreshToken
-)
+);
 
 router.post(
      '/change-password',
      auth(
+          UserRole.SUPER_ADMIN,
           UserRole.ADMIN,
           UserRole.DOCTOR,
           UserRole.PATIENT
@@ -39,8 +38,30 @@ router.post(
 
 router.post(
      '/reset-password',
+     (req: Request, res: Response, next: NextFunction) => {
+
+     //user is resetting password without token and logged in newly created admin or doctor
+     if (!req.headers.authorization && req.cookies.accessToken) {
+          console.log(req.headers.authorization, "from reset password route guard");
+          console.log(req.cookies.accessToken, "from reset password route guard");
+          auth(
+               UserRole.SUPER_ADMIN,
+               UserRole.ADMIN,
+               UserRole.DOCTOR,
+               UserRole.PATIENT
+          )(req, res, next);
+          } else {
+          //user is resetting password via email link with token
+          next();
+     }
+     },
      AuthController.resetPassword
-)
+);
+
+router.get(
+     "/me",
+     AuthController.getMe
+);
 
 
 

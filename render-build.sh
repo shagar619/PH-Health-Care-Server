86b67c09f10c2fef3f3645f2358364dc0443df7b
shagar619 <ashagar619@gmail.com>
2026-02-27 21:40:13 +0600

    Fix build by including dev dependencies

--- render-build.sh
+++ render-build.sh
@@ -3,7 +3,9 @@
 # set on error
 set -o errexit
 
-npm install
+# Force npm to install devDependencies so TypeScript has its types
+npm install --include=dev
+
 npm run build
 npx prisma generate
 npx prisma migrate deploy
